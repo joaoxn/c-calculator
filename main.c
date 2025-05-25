60ad42c23fdec34e02cf574dd4414b18b853f8f8
João Victor Olivo <jvso@yahoo.com>
2025-05-25 12:22:44 -0300

    Abstracted operations to single 'operation' function

--- main.c
+++ main.c
@@ -2,68 +2,45 @@
 #include <stdio.h>
 #include <windows.h>
 
-void addition() {
+void operation(char name[], float (*func)(float, float)) {
     printf("\n");
-    printf("============================\n");
-    printf("Calculadora Simples - ADIÇÃO\n");
-    printf("============================\n");
+
+    const size_t name_len = strlen(name);
+
+    printf("====================");
+    for (size_t i = 0; i < name_len; i++) {
+        printf("=");
+    }
+    printf("\nCalculadora Simples - %s\n", name);
+    printf("====================");
+    for (size_t i = 0; i < name_len; i++) {
+        printf("=");
+    }
 
     float n1, n2;
-    printf("Digite o primeiro número: ");
+    printf("\nDigite o primeiro número: ");
     scanf("%f", &n1);
     printf("Digite o segundo número: ");
     scanf("%f", &n2);
 
     printf("\n");
-    printf("O resultado é: %f\n", n1 + n2);
+    printf("O resultado é: %f\n", func(n1, n2));
 }
 
-void subtraction() {
-    printf("\n");
-    printf("===============================\n");
-    printf("Calculadora Simples - SUBTRAÇÃO\n");
-    printf("===============================\n");
-
-    float n1, n2;
-    printf("Digite o primeiro número: ");
-    scanf("%f", &n1);
-    printf("Digite o segundo número: ");
-    scanf("%f", &n2);
-
-    printf("\n");
-    printf("O resultado é: %f\n", n1 - n2);
+float add(const float n1, const float n2) {
+    return n1 + n2;
 }
 
-void multiplication() {
-    printf("\n");
-    printf("===================================\n");
-    printf("Calculadora Simples - MULTIPLICAÇÃO\n");
-    printf("===================================\n");
-
-    float n1, n2;
-    printf("Digite o primeiro número: ");
-    scanf("%f", &n1);
-    printf("Digite o segundo número: ");
-    scanf("%f", &n2);
-
-    printf("\n");
-    printf("O resultado é: %f\n", n1 * n2);
+float subtract(const float n1, const float n2) {
+    return n1 - n2;
 }
 
-void division() {
-    printf("\n");
-    printf("=============================\n");
-    printf("Calculadora Simples - DIVISÃO\n");
-    printf("=============================\n");
+float multiply(const float n1, const float n2) {
+    return n1 * n2;
+}
 
-    float n1, n2;
-    printf("Digite o primeiro número: ");
-    scanf("%f", &n1);
-    printf("Digite o segundo número: ");
-    scanf("%f", &n2);
-
-    printf("\n");
-    printf("O resultado é: %f\n", n1 / n2);
+float divide(const float n1, const float n2) {
+    return n1 / n2;
 }
 
 void menu() {
@@ -85,11 +62,11 @@ void menu() {
 
         printf("\n");
         switch (option) {
-            case 1: addition(); break;
-            case 2: subtraction(); break;
-            case 3: multiplication(); break;
-            case 4: division(); break;
-            case 0: exiting = 1; break;
+            case 1: operation("ADIÇÃO", add);                    break;
+            case 2: operation("SUBTRAÇÃO", subtract);            break;
+            case 3: operation("MULTIPLICAÇÃO", multiply);        break;
+            case 4: operation("DIVISÃO", divide);                break;
+            case 0: exiting = 1;                                      break;
             default: printf("'%d' não é uma opção!\n", option); break;
         }
     }
